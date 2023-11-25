5698580588f8ebaf4a834bfdc8a9378d7fbd8371
ginga <izuto.kudo.cop@gmail.com>
2023-11-25 18:21:40 +0900

    SQLの応用

---
+++ src/dbSample/DbConnectSample01.java
@@ -0,0 +1,81 @@
+package dbSample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnectSample01 {
+
+    public static void main(String[] args) {
+        // データベース接続と結果取得のための変数
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        // 1. ドライバのクラスをJava上で読み込む
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+        // 2. DBと接続する
+        con = DriverManager.getConnection(
+              "jdbc:mysql://localhost/world?useSSL=false",
+              "root",
+              "rootroot"
+              );
+
+        // 3. DBとやりとりする窓口（Statementオブジェクト）の作成
+        stmt = con.createStatement();
+
+        // 4, 5. Select文の実行と結果を格納／代入
+        String sql = "select * from country limit 50";
+        rs = stmt.executeQuery(sql);
+
+        // 6. 結果を表示する
+        while(rs.next()) {
+           // Name列の値を取得
+            String name = rs.getString("Name");
+           // Population列の値を取得
+            int population = rs.getInt("Population");
+           // 取得した値を表示
+            System.out.println(name);
+            System.out.println(population);
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("JDBCドライバのロードに失敗しました。");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("データベースに異常が発生しました。");
+            e.printStackTrace();
+        } finally {
+         // 7. 接続を閉じる
+            if( rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e){
+                    System.err.println("ResultSetを閉じるときにエラーが発生しました");
+                    e.printStackTrace();
+                }
+             }
+            if(stmt != null) {
+               try {
+                   stmt.close();
+               } catch (SQLException e){
+                   System.err.println("Statementを閉じるときにエラーが発生しました");
+                   e.printStackTrace();
+               }
+            }
+            if(con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e){
+                    System.err.println("データベース切断時にエラーが発生しました");
+                    e.printStackTrace();
+                }
+             }
+
+    }
+}
+}
